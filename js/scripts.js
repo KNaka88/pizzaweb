aad82d2fbb091a57340eec4fcf3e2c1fec9bd614
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-27 09:05:33 -0800

    add basic structure of constructor and prototype on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
 $(function(){
 
 //Constractor
-  //pizza
-   //size
-   //toppings
-   //type
-   //price
+function Pizza (pizzaName, pizzaSize, pizzaToppings){
+   this.pizzaName = pizzaName;
+   this.pizzaSize = pizzaSize;
+   this.pizzaToppings = pizzaToppings;
+   this.price = 0;
+   this.quantity = 0;
+}
+
+function UserCart(pizza){
+
+}
+
+
+//prototype
+Pizza.prototype.discount = function(){
+
+}
+Pizza.prototype.promotion = function(){
+
+}
 
 //Gloval Variables
-
+var totalPrice = 0;
+var totalQuantity = 0;
 
 
 //Business Logic Goes Here
 
   //STEP1: when user click "add cart", get inputs and create object
+    
+
 
   //STEP2: if user didn't provide sufficient info, alert.
 
