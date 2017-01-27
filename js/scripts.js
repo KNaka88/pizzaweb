3d70dd821da2fcb53299cb4b3986568db01faf84
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-27 10:34:33 -0800

    add logic for STEP1: get pizza1 info when user click add button, update HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(function(){
 
 //Constractor
-function Pizza (pizzaName, pizzaSize, pizzaToppings){
+function Pizza (pizzaName, pizzaSize, pizzaToppings, pizzaPrice, pizzaQuantity){
    this.pizzaName = pizzaName;
    this.pizzaSize = pizzaSize;
    this.pizzaToppings = pizzaToppings;
-   this.price = 0;
-   this.quantity = 0;
+   this.pizzaPrice = pizzaPrice;
+   this.pizzaQuantity = pizzaQuantity;
 }
 
 function UserCart(pizza){
@@ -23,15 +23,44 @@ Pizza.prototype.promotion = function(){
 }
 
 //Gloval Variables
+
+var pizzaName = "";
+var pizzaSize = "";
+var pizzaToppings = [];
+var pizzaPrice = 0;
+var pizzaQuantity = 0;
 var totalPrice = 0;
 var totalQuantity = 0;
-
+var pizza1 = new Pizza();
 
 //Business Logic Goes Here
 
   //STEP1: when user click "add cart", get inputs and create object
-    
 
+  $("#pizza1 button").click(function(){
+    pizzaName = document.getElementById("pizza1-name").textContent;
+
+    pizzaSize = $("#pizza1 input:radio[name=pizza1-size]:checked").val();
+
+    //remove duplicates!!
+    $("#pizza1 input:checkbox[name=pizza1-topping]:checked").each(function(){
+      pizzaToppings.push($(this).val());
+    });
+
+    pizzaPrice = parseInt(document.getElementById("pizza1-price").textContent);
+
+    pizzaQuantity = parseInt($("#pizza1-quantity").val());
+
+    console.log("pizzaName = " +  pizzaName);
+    console.log("pizzaSize = " + pizzaSize);
+    console.log("pizzaToppings = " + pizzaToppings);
+    console.log("pizzaPrice = " + pizzaPrice);
+    console.log("pizzaQuantity = " + pizzaQuantity);
+
+    pizza1 = new Pizza(pizzaName, pizzaSize, pizzaToppings, pizzaPrice, pizzaQuantity);
+
+    console.log(pizza1);
+  });
 
   //STEP2: if user didn't provide sufficient info, alert.
 
