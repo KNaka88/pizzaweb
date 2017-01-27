bf8c043eec376ff1e213d362e8d93b31fbb17c49
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-27 08:30:39 -0800

    add comment for basic structure of JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 $(function(){
 
+//Constractor
+
+  //pizza
+   //size
+   //toppings
+   //type
+   //price
+
+//Gloval Variables
+
+
+
 //Business Logic Goes Here
 
+  //STEP1: when user click "add cart", get inputs and create object
 
+  //STEP2: if user didn't provide sufficient info, alert.
 
+  //STEP3: Given user inputs, call prototype and calculate the price
 
+  //STEP4: When user click purchase, show the order summary and open payment form
 
-
-
-
+  //STEP5: when user type the payment info and buy button show the purchase summary
 
 
 //Interface Logic Goes Here
@@ -18,6 +32,9 @@ $(function(){
 
 
 
-
-
 });
+
+//Bonus
+  //COUPON CODE form
+  //Pop up window (Modal)
+  //another order confirmation page
